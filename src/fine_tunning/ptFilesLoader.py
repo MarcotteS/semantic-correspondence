7738efd74e73b5f3a257d579949f509bca60454c
guillaume DB <guillaume@isae.fr>
2026-01-29 12:59:38 +0100

    mini commit

--- src/fine_tunning/ptFilesLoader.py
+++ src/fine_tunning/ptFilesLoader.py
@@ -19,7 +19,8 @@ import os
 import torch.nn.functional as F
 import numpy as np
 import matplotlib.pyplot as plt
-import CorrespondenceMatcher2
+from CorrespondenceMatcher2 import CorrespondenceMatcher2
+
 
 def load_matcher_from_drive_ckpt(
     ckpt_path: str,
